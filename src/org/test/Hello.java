9109429d8cf7a3d887d84a834c978e20c5ce0771
Jaishashylu <shylujaisha@gmail.com>
2021-11-01 23:33:18 +0530

    YYY

---
+++ src/org/test/Hello.java
@@ -0,0 +1,28 @@
+package org.test;
+
+public class Hello {
+	public static void main(String[] args) {
+		String s="jaisha";
+		String s1="SHYLU";
+	char charAt=s.charAt(2);	
+		System.out.println(charAt);
+	int indexOf=s.indexOf('s');
+	System.out.println(indexOf);
+	String toUpperCase=s.toUpperCase();
+	System.out.println(toUpperCase);
+	String toLowerCase=s1.toLowerCase();
+	System.out.println(toLowerCase);
+	int length=s.length();
+	System.out.println(length);
+	boolean startsWith=s.startsWith("jai");
+	System.out.println(startsWith);
+	boolean endsWith=s1.endsWith("lu");
+	System.out.println(endsWith);
+	
+	
+		
+		
+		
+		
+		
+}}
